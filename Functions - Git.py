24c68b6b9dd7fee085d3dd601784fc8409ac3859
Corey Herubin <02corey.herubin@gulllakecs.org>
2019-02-11 09:54:31 -0500

    Add a Greetings Function

---
+++ Functions - Git.py
@@ -0,0 +1,11 @@
+# 4.13.3: Greeting
+# Corey Herubin
+# 2.6.19
+
+name = input("What is your name: ")
+
+def greeting():
+    print("Hi there " + name + "!")
+    print("Nice to meet you!")
+
+greeting()
